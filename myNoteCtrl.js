c93e4f837d9dab6cf55b4c7c662e0958d7c5dca3
Katie McTaggart <katiemctaggartengineer@gmail.com>
2022-08-31 13:59:56 -0400

    Add a remaining character counter to textbox

--- myNoteCtrl.js
+++ myNoteCtrl.js
@@ -1,6 +1,8 @@
 app.controller("myNoteCtrl", function($scope) {
     $scope.message = "";
-
+    $scope.left = function() {
+        return 100 - $scope.message.length;
+    };
     $scope.clear = function() {
         $scope.message = "";
     };
